55932ebef150069333366e6df863a4d9109bba65
JohariSahil <sahiljohari04@gmail.com>
2024-11-19 12:45:17 +0530

    added loading spinner for get recruiter

--- src/pages/Recruiter/OnboardingList.tsx
+++ src/pages/Recruiter/OnboardingList.tsx
@@ -4,8 +4,14 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect, useState } from 'react';
 import { getAllRecruiters } from '../../services/recruiterService';
+import Loader from '../../common/Loader';
 
 const OnboardingList = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  //   useEffect(() => {
+  //     setTimeout(() => setLoading(false), 1000);
+  //   }, []);
+
   const [onboardingForms, setOnboardingForms] = useState<
     {
       id: string;
@@ -38,6 +44,8 @@ const OnboardingList = () => {
           pauseOnHover: true,
           draggable: true,
         });
+      } finally {
+        setLoading(false); // Set loading to false after fetching
       }
     };
 
@@ -91,77 +99,86 @@ const OnboardingList = () => {
     <>
       <Breadcrumb pageName="OnBoarding List" />
 
-      <div className="flex flex-col gap-9">
-        {onboardingForms.length > 0 ? (
-          onboardingForms.map((form) => (
-            <div
-              key={form.id}
-              className="relative rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"
-            >
-              <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-                <h3 className="font-medium text-black dark:text-white">
-                  Recruiter OnBoarding Form
-                </h3>
-              </div>
-              {/* Close Button */}
-              <button
-                onClick={() => removeForm(form.id)}
-                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-500 text-2xl p-2"
+      {loading ? (
+        <div
+          className="flex justify-center items-start h-full"
+          style={{ backgroundColor: 'transparent' }}
+        >
+          <Loader /> {/* Show loader while loading */}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-9">
+          {onboardingForms.length > 0 ? (
+            onboardingForms.map((form) => (
+              <div
+                key={form.id}
+                className="relative rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"
               >
-                &times;
-              </button>
+                <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+                  <h3 className="font-medium text-black dark:text-white">
+                    Recruiter OnBoarding Form
+                  </h3>
+                </div>
+                {/* Close Button */}
+                <button
+                  onClick={() => removeForm(form.id)}
+                  className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-500 text-2xl p-2"
+                >
+                  &times;
+                </button>
 
-              <div className="p-6.5">
-                <h3 className="font-medium text-black dark:text-white">
-                  Recruiter Summary
-                </h3>
-                <p>First Name: {form.firstName}</p>
-                <p>Last Name: {form.lastName}</p>
-                <p>Email: {form.email}</p>
-                <p>
-                  Status:{' '}
-                  <span
-                    className={`font-semibold ${
-                      form.status === 'Completed'
-                        ? ' text-red-500'
-                        : 'text-gray-500'
-                    }`}
-                  >
-                    {form.status}
-                  </span>
-                </p>
-                <div className="flex gap-4 mt-4">
-                  {form.status === 'Completed' ? (
-                    <NavLink
-                      to="/recruiter-master/onboarding-list/display-onboarding-form"
+                <div className="p-6.5">
+                  <h3 className="font-medium text-black dark:text-white">
+                    Recruiter Summary
+                  </h3>
+                  <p>First Name: {form.firstName}</p>
+                  <p>Last Name: {form.lastName}</p>
+                  <p>Email: {form.email}</p>
+                  <p>
+                    Status:{' '}
+                    <span
+                      className={`font-semibold ${
+                        form.status === 'Completed'
+                          ? ' text-red-500'
+                          : 'text-gray-500'
+                      }`}
+                    >
+                      {form.status}
+                    </span>
+                  </p>
+                  <div className="flex gap-4 mt-4">
+                    {form.status === 'Completed' ? (
+                      <NavLink
+                        to="/recruiter-master/onboarding-list/display-onboarding-form"
+                        className="rounded bg-primary p-2 font-medium text-gray hover:bg-opacity-90"
+                      >
+                        View Form
+                      </NavLink>
+                    ) : (
+                      <button
+                        disabled
+                        className="rounded bg-gray-300 p-2 font-medium text-gray cursor-not-allowed"
+                      >
+                        View Form
+                      </button>
+                    )}
+                    <button
+                      onClick={() => copyToClipboard(form.id)}
                       className="rounded bg-primary p-2 font-medium text-gray hover:bg-opacity-90"
                     >
-                      View Form
-                    </NavLink>
-                  ) : (
-                    <button
-                      disabled
-                      className="rounded bg-gray-300 p-2 font-medium text-gray cursor-not-allowed"
-                    >
-                      View Form
+                      Get Link
                     </button>
-                  )}
-                  <button
-                    onClick={() => copyToClipboard(form.id)}
-                    className="rounded bg-primary p-2 font-medium text-gray hover:bg-opacity-90"
-                  >
-                    Get Link
-                  </button>
+                  </div>
                 </div>
               </div>
+            ))
+          ) : (
+            <div className="text-center text-gray-500">
+              <p>No onboarding forms available.</p>
             </div>
-          ))
-        ) : (
-          <div className="text-center text-gray-500">
-            <p>No onboarding forms available.</p>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
