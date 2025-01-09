a6da2520f6fceb098dc1f2b33d930b39b571063c
JohariSahil <sahiljohari04@gmail.com>
2025-01-09 13:43:23 +0530

    remove some code from signIn.tsx

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
-import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 import LogoDark from '../../images/logo/logo-dark.svg';
 import Logo from '../../images/logo/logo.svg';
 
